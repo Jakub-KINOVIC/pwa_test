2a536aa0ecbe056f7aee0b17e4459c99fcfa11a0
jkinovic <167547732+Jakub-KINOVIC@users.noreply.github.com>
2024-12-04 14:47:09 +0100

    Add files

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'hello-pwa-cache';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install event: caching files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event: serving files from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
